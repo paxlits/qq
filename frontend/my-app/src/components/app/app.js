4d7242fe4cc318c9d66dacab10fd7dd00cb533bd
levchik <malaninaaaa@gmail.com>
2025-03-01 00:08:29 +0300

    made 10% hander

---
+++ frontend/my-app/src/components/app/app.js
@@ -0,0 +1,13 @@
+import AppHendler from "../appHeader/appHendler";
+
+
+function App(){
+    return(
+        <div className="app">
+            <AppHendler/>
+        </div>
+        
+    )
+}
+
+export default App;
